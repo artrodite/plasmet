48d1e89b04fbe9ad264213d8d91d36b48f931391
Szqii <szqii.altn@gmail.com>
2022-12-04 18:55:49 +0300

    feat : create views

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,60 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="about-text">
+      <h2>Hakkımızda</h2>
+      <span
+        >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure
+        voluptates minima vitae eaque. Est at ipsum mollitia eaque. Fugiat
+        magnam, saepe quidem dolorum porro, rem odit autem quasi officiis id
+        eligendi optio ex fugit laboriosam consectetur praesentium veritatis et
+        excepturi in architecto blanditiis culpa! Dolor eum alias at ipsam quia
+        perferendis, quidem officia doloremque obcaecati adipisci asperiores
+        corporis laboriosam illo rem mollitia maxime consequuntur nobis labore
+        est itaque sapiente, iure a. Veritatis ipsam adipisci optio quasi
+        recusandae error exercitationem unde voluptate voluptates cum nisi
+        dignissimos accusantium eos ea repellendus animi, natus, neque molestias
+        molestiae nulla beatae modi dolor? Aspernatur esse, quia libero, nostrum
+        reprehenderit dolor corrupti debitis nulla veritatis corporis
+        accusantium. Voluptate, beatae. Laudantium sit dolorum minus ex
+        repellendus possimus!</span
+      >
+    </div>
+    <img src="../assets/about-bg.png" class="about-bg" alt="" />
   </div>
 </template>
+
+<script></script>
+
+<style lang="scss" scoped>
+@import "../style/_variables.scss";
+
+.about {
+  height: 100vh;
+  // background-image: url("../assets/about-bg.png");
+  // background-position: top right;
+  // background-repeat: no-repeat;
+  padding: 0 110px;
+  .about-bg {
+    position: fixed;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  .about-text {
+    width: 500px;
+    margin-top: 247px;
+    color: $light-text-color;
+
+    h2 {
+      font-size: 48px;
+      font-weight: 700;
+      letter-spacing: 4px;
+    }
+
+    span {
+      font-size: 20px;
+      font-weight: 400;
+    }
+  }
+}
+</style>
---
+++ src/views/ServicesView.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="services">
+    <div class="services-text">
+      <h2>Hizmetlerimiz</h2>
+      <div class="service-text">
+        <h3>Montaj</h3>
+        <span
+          >Anahtar teslim projelerimizde tüm sertifikasyon eğitimlerini
+          tamamlamış uzman ekiplerimiz iş güvenliği tedbirlerini alarak mümkün
+          olan en kısa sürede sızdırmazlık garantisi, kaliteli işçilik
+          sunmaktadır.</span
+        >
+      </div>
+      <div class="service-text">
+        <h3>Projelendirme & Danışmanlık</h3>
+        <span
+          >Deneyimli mühendis ekibimiz projenize uygun olarak ihtiyaçlarınızı
+          doğru belirlemek, gereksiz masraflar yapılmasını önlemek için
+          hizmetinizde.</span
+        >
+      </div>
+      <div class="service-text">
+        <h3>Süpervizörlük ve Devreye Alma</h3>
+        <span
+          >Uygulama sırasında yapılan hataların maliyeti tahmin edildiğinden çok
+          fazla olabilir. Hataları minimuma indirmek için deneyimli
+          mühendislerimiz yanınızda.</span
+        >
+      </div>
+      <div class="service-text">
+        <h3>Bakım</h3>
+        <span
+          >Periyodik bakım gereken ürünler için uygun takvim çerçevesinde bakım
+          hizmetimizden yararlanabilirsiniz.</span
+        >
+      </div>
+    </div>
+
+    <div class="insurance">
+      <h2>Ürün ve hizmetlerimiz güvence altında.</h2>
+      <span>Üretimden montaja her aşamada üretici sorumluluğu.</span>
+      <div class="insurance-text-container">
+        <div class="insurance-text">
+          <img src="../assets/insurance-icon.png" alt="" />Sızdırmazlık
+          garantisi
+        </div>
+        <div class="insurance-text">
+          <img src="../assets/insurance-icon.png" alt="" />Darbeye dayanım
+          garantisi.
+        </div>
+        <div class="insurance-text">
+          <img src="../assets/insurance-icon.png" alt="" />Açkı mekanizması
+          garantisi.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped>
+.services {
+  background-image: url("../assets/services-bg.png");
+  background-position: top right;
+  background-repeat: no-repeat;
+  padding: 0 110px;
+  letter-spacing: 4px;
+
+  h2 {
+    font-size: 48px;
+    font-weight: 700;
+  }
+
+  h3 {
+    font-size: 32px;
+    font-weight: 500;
+  }
+  span {
+    font-size: 20px;
+  }
+
+  .services-text {
+    margin-top: 247px;
+    width: 840px;
+
+    .service-text {
+      margin-bottom: 35px;
+    }
+  }
+
+  .insurance {
+    margin-top: 90px;
+    margin-bottom: 200px;
+
+    .insurance-text {
+      display: flex;
+      align-items: center;
+      margin: 30px 0;
+      font-size: 24px;
+      img {
+        margin-right: 50px;
+      }
+    }
+  }
+}
+</style>
